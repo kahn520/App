9a5f3fa3350bb2dce2eeabc43a9c17292b7aa386
misear <zhong.ch22@gmail.com>
2016-01-21 09:10:10 +0800

    v

---
+++ FileScaner/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileScaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strPath = "G:\\美化大师背景模板";
+            string strSavePath = @"G:\App\Template\";
+            string[] strFiles = Directory.GetFiles(strPath, "*", SearchOption.AllDirectories);
+            strFiles = strFiles.Where(f => f.EndsWith(".pptx") && f.IndexOf("KPBG") > 0).ToArray();
+            int i = 0;
+            int count = strFiles.Length;
+            foreach (string file in strFiles)
+            {
+                string strNewPath = strSavePath + Path.GetFileName(file);
+                if(File.Exists(strNewPath))
+                    continue;
+                File.Move(file, strSavePath + Path.GetFileName(file));
+                Console.WriteLine((++i) + "/" + count);
+            }
+        }
+    }
+}
---
+++ TemplateClern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Core;
+using Microsoft.Office.Interop.PowerPoint;
+using Shape = Microsoft.Office.Interop.PowerPoint.Shape;
+
+namespace TemplateClern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strFile = @"G:\APP\A000120140530A01KPBG.pptx";
+            Application app = new Application();
+            _Presentation ppt = app.Presentations.Open(strFile, MsoTriState.msoFalse, MsoTriState.msoFalse, MsoTriState.msoFalse);
+            ppt.SlideMaster.
+            ppt.Save();
+            ppt.Close();
+            app.Quit();
+        }
+    }
+}
